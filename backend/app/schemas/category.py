ffcce1516a10fe9fb2b2e1b19914f939d1b8548f
yus <yus@i57597.istu.int>
2026-01-30 11:13:06 +0800

    add small fixes

--- backend/app/schemas/category.py
+++ backend/app/schemas/category.py
@@ -13,5 +13,5 @@ class CategoryResponse(CategoryBase):
     id: int = Field(..., description="Unique category identifier")
     
     class Config:
-        form_attributes = True
+        from_attributes = True
         
